30e317e1230293c7626412e056217ef2b2b9dc89
John Weldon <johnweldon4@gmail.com>
2021-04-02 17:06:34 -0700

    Cleanup

--- main.go
+++ main.go
@@ -7,14 +7,18 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+
+	"github.com/johnweldon/unifi-scheduler/unifi"
 )
 
-var useJSON bool
+var useJSON bool // nolint
 
+// nolint
 func init() {
 	flag.BoolVar(&useJSON, "json", useJSON, "output as json")
 }
 
+// nolint:funlen
 func main() {
 	flag.Parse()
 
@@ -23,33 +27,40 @@ func main() {
 	username := os.Getenv("UNIFI_USERNAME")
 	password := os.Getenv("UNIFI_PASSWORD")
 	endpoint := os.Getenv("UNIFI_ENDPOINT")
-	ses := &UnifiSession{
+	ses := &unifi.Session{
 		Username: username,
 		Password: password,
 		Endpoint: endpoint,
+		UseJSON:  useJSON,
 	}
 
 	if err := ses.Initialize(); err != nil {
 		log.Printf("Error:\n%v", err)
+
 		return
 	}
+
 	fmt.Fprintf(os.Stderr, "Initialized...\n")
 
 	if msg, err := ses.Login(); err != nil {
 		log.Printf("Login Error: (%s)\n%v", msg, err)
+
 		return
 	}
+
 	fmt.Fprintf(os.Stderr, "Logged in...\n")
 
 	u, err := ses.ListClients()
 	if err != nil {
 		log.Printf("Error:\n%v", err)
+
 		return
 	}
 
-	users := &Response{}
-	if err := json.Unmarshal([]byte(u), users); err != nil {
+	var users unifi.Response
+	if err := json.Unmarshal([]byte(u), &users); err != nil {
 		log.Printf("%s\n%v", u, err)
+
 		return
 	}
 
@@ -58,71 +69,16 @@ func main() {
 		target[name] = true
 	}
 
-	functions := map[string]func([]Client, map[string]bool){
-		"list":    listFn,
-		"block":   ses.blockFn,
-		"unblock": ses.unblockFn,
+	functions := map[string]func([]unifi.Client, map[string]bool){
+		"list":    ses.ListFn,
+		"block":   ses.BlockFn,
+		"unblock": ses.UnblockFn,
 	}
 
 	fn, ok := functions[invocation]
 	if !ok {
-		fn = listFn
+		fn = ses.ListFn
 	}
 
 	fn(users.Data, target)
-}
-
-func listFn(clients []Client, keys map[string]bool) {
-	if useJSON {
-		if err := json.NewEncoder(os.Stdout).Encode(clients); err != nil {
-			log.Printf("error encoding JSON: %v", err)
-		}
-		return
-	}
-
-	for _, client := range clients {
-		display := firstNonEmpty(client.Name, client.Hostname, "-")
-		ip := firstNonEmpty(client.FixedIP, client.IP)
-		log.Printf("%30s %s %s", display, client.MAC, ip)
-	}
-}
-
-func (s *UnifiSession) blockFn(clients []Client, keys map[string]bool) {
-	for _, client := range clients {
-		display := firstNonEmpty(client.Name, client.Hostname)
-		if k, ok := keys[display]; ok && k {
-			res, err := s.Block(client.MAC)
-			if err != nil {
-				log.Printf("%s\nerror blocking: %v", res, err)
-				return
-			}
-
-			log.Printf("%s\n", res)
-		}
-	}
-}
-
-func (s *UnifiSession) unblockFn(clients []Client, keys map[string]bool) {
-	for _, client := range clients {
-		display := firstNonEmpty(client.Name, client.Hostname)
-		if k, ok := keys[display]; ok && k {
-			res, err := s.Unblock(client.MAC)
-			if err != nil {
-				log.Printf("%s\nerror unblocking: %v", res, err)
-				return
-			}
-
-			log.Printf("%s\n", res)
-		}
-	}
-}
-
-func firstNonEmpty(s ...string) string {
-	for _, candidate := range s {
-		if len(candidate) > 0 {
-			return candidate
-		}
-	}
-
-	return ""
 }
--- unifi/session.go
+++ unifi/session.go
@@ -1,15 +1,24 @@
-package main
+package unifi
 
 import (
 	"bytes"
+	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
+	"os"
 	"time"
+)
+
+var (
+	ErrNilSession           = errors.New("nil session")
+	ErrUninitializedSession = errors.New("uninitialized session")
 )
 
 // Response encapsulates a UniFi http response.
@@ -43,11 +52,12 @@ type Client struct {
 	UseFixedIP          bool   `json:"use_fixedip,omitempty"`
 }
 
-// UnifiSession wraps metadata to manage session state.
-type UnifiSession struct {
+// Session wraps metadata to manage session state.
+type Session struct {
 	Endpoint string
 	Username string
 	Password string
+	UseJSON  bool
 	csrf     string
 	client   *http.Client
 	login    func() (string, error)
@@ -55,9 +65,9 @@ type UnifiSession struct {
 }
 
 // Initialize prepares the session for use.
-func (s *UnifiSession) Initialize() error {
+func (s *Session) Initialize() error {
 	if s == nil {
-		return errors.New("nil session")
+		return ErrNilSession
 	}
 
 	s.err = nil
@@ -65,9 +75,11 @@ func (s *UnifiSession) Initialize() error {
 	if len(s.Endpoint) == 0 {
 		s.setErrorString("missing endpoint")
 	}
+
 	if len(s.Username) == 0 {
 		s.setErrorString("missing username")
 	}
+
 	if len(s.Password) == 0 {
 		s.setErrorString("missing password")
 	}
@@ -76,63 +88,104 @@ func (s *UnifiSession) Initialize() error {
 	if err != nil {
 		s.setError(err)
 	}
-	s.client = &http.Client{
+
+	s.client = &http.Client{ // nolint:exhaustivestruct
 		Jar:     jar,
 		Timeout: time.Minute * 1,
 	}
 	s.login = s.webLogin
+
 	return s.err
 }
 
 // Login performs authentication with the UniFi server, and stores the
 // http credentials.
-func (s *UnifiSession) Login() (string, error) {
+func (s *Session) Login() (string, error) {
 	if s.login == nil {
 		s.login = func() (string, error) {
-			return "", errors.New("uninitialized session")
+			return "", ErrUninitializedSession
 		}
 	}
+
 	return s.login()
 }
 
 // ListClients describes the known UniFi clients.
-func (s *UnifiSession) ListClients() (string, error) {
+func (s *Session) ListClients() (string, error) {
 	if s.err != nil {
 		return "", s.err
 	}
+
 	u, err := url.Parse(fmt.Sprintf("%s/proxy/network/api/s/default/rest/user", s.Endpoint))
 	if err != nil {
 		s.setError(err)
+
 		return "", s.err
 	}
+
 	return s.get(u)
 }
 
 // Kick disconnects a connected client, identified by MAC address.
-func (s *UnifiSession) Kick(mac string) (string, error) {
+func (s *Session) Kick(mac string) (string, error) {
 	return s.macAction("kick-sta", mac)
 }
 
 // Block prevents a specific client (identified by MAC) from connecting
 // to the UniFi network.
-func (s *UnifiSession) Block(mac string) (string, error) {
+func (s *Session) Block(mac string) (string, error) {
 	return s.macAction("block-sta", mac)
 }
 
 // Unblock re-enables a specific client.
-func (s *UnifiSession) Unblock(mac string) (string, error) {
+func (s *Session) Unblock(mac string) (string, error) {
 	return s.macAction("unblock-sta", mac)
 }
 
-func (s *UnifiSession) webLogin() (string, error) {
+func (s *Session) BlockFn(clients []Client, keys map[string]bool) {
+	for _, client := range clients {
+		display := firstNonEmpty(client.Name, client.Hostname)
+		if k, ok := keys[display]; ok && k {
+			res, err := s.Block(client.MAC)
+			if err != nil {
+				log.Printf("%s\nerror blocking: %v", res, err)
+
+				return
+			}
+
+			log.Printf("%s\n", res)
+		}
+	}
+}
+
+func (s *Session) UnblockFn(clients []Client, keys map[string]bool) {
+	for _, client := range clients {
+		display := firstNonEmpty(client.Name, client.Hostname)
+		if k, ok := keys[display]; ok && k {
+			res, err := s.Unblock(client.MAC)
+			if err != nil {
+				log.Printf("%s\nerror unblocking: %v", res, err)
+
+				return
+			}
+
+			log.Printf("%s\n", res)
+		}
+	}
+}
+
+func (s *Session) webLogin() (string, error) {
 	if s.err != nil {
 		return "", s.err
 	}
+
 	u, err := url.Parse(fmt.Sprintf("%s/api/auth/login", s.Endpoint))
 	if err != nil {
 		s.setError(err)
+
 		return "", s.err
 	}
+
 	r := bytes.NewBufferString(
 		fmt.Sprintf(
 			`{"username":%q,"password":%q,"strict":"true","remember":"true"}`,
@@ -142,67 +195,83 @@ func (s *UnifiSession) webLogin() (string, error) {
 	if err == nil {
 		s.login = func() (string, error) { return respBody, nil }
 	}
+
 	return respBody, err
 }
 
-func (s *UnifiSession) macAction(action string, mac string) (string, error) {
+func (s *Session) macAction(action string, mac string) (string, error) {
 	if b, err := s.login(); err != nil {
 		return b, err
 	}
+
 	u, err := url.Parse(fmt.Sprintf("%s/proxy/network/api/s/default/cmd/stamgr", s.Endpoint))
 	if err != nil {
 		s.setError(err)
+
 		return "", s.err
 	}
+
 	r := bytes.NewBufferString(fmt.Sprintf(`{"cmd":%q,"mac":%q}`, action, mac))
+
 	return s.post(u, r)
 }
 
-func (s *UnifiSession) get(u *url.URL) (string, error) {
+func (s *Session) get(u fmt.Stringer) (string, error) {
 	return s.verb("GET", u, nil)
 }
 
-func (s *UnifiSession) post(u *url.URL, body io.Reader) (string, error) {
+func (s *Session) post(u fmt.Stringer, body io.Reader) (string, error) {
 	return s.verb("POST", u, body)
 }
 
-func (s *UnifiSession) verb(verb string, u *url.URL, body io.Reader) (string, error) {
-	req, err := http.NewRequest(verb, u.String(), body)
+func (s *Session) verb(verb string, u fmt.Stringer, body io.Reader) (string, error) {
+	req, err := http.NewRequestWithContext(context.Background(), verb, u.String(), body)
 	if err != nil {
 		s.setError(err)
+
 		return "", s.err
 	}
+
 	req.Header.Set("User-Agent", "unifibot 2.0")
 	req.Header.Set("Content-Type", "application/json; charset=utf-8")
 	req.Header.Set("Accept", "application/json")
 	req.Header.Set("Origin", s.Endpoint)
+
 	if s.csrf != "" {
 		req.Header.Set("x-csrf-token", s.csrf)
 	}
+
 	resp, err := s.client.Do(req)
 	if err != nil {
 		s.setError(err)
+
 		return "", s.err
 	}
 	defer resp.Body.Close()
+
 	if tok := resp.Header.Get("x-csrf-token"); tok != "" {
 		s.csrf = tok
 	}
+
 	respBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		s.setError(err)
+
 		return "", s.err
 	}
+
 	if resp.StatusCode < http.StatusOK || http.StatusBadRequest <= resp.StatusCode {
 		s.setErrorString(http.StatusText(resp.StatusCode))
 	}
+
 	return string(respBody), s.err
 }
 
-func (s *UnifiSession) setError(e error) {
+func (s *Session) setError(e error) {
 	if e == nil {
 		return
 	}
+
 	if s.err == nil {
 		s.err = fmt.Errorf("%w", e)
 	} else {
@@ -210,13 +279,40 @@ func (s *UnifiSession) setError(e error) {
 	}
 }
 
-func (s *UnifiSession) setErrorString(e string) {
+func (s *Session) setErrorString(e string) {
 	if len(e) == 0 {
 		return
 	}
+
 	if s.err == nil {
-		s.err = fmt.Errorf("%s", e)
+		s.err = fmt.Errorf("%s", e) // nolint:goerr113
 	} else {
 		s.err = fmt.Errorf("%s\n%w", e, s.err)
 	}
+}
+
+func (s *Session) ListFn(clients []Client, _ map[string]bool) {
+	if s.UseJSON {
+		if err := json.NewEncoder(os.Stdout).Encode(clients); err != nil {
+			log.Printf("error encoding JSON: %v", err)
+		}
+
+		return
+	}
+
+	for _, client := range clients {
+		display := firstNonEmpty(client.Name, client.Hostname, "-")
+		ip := firstNonEmpty(client.FixedIP, client.IP)
+		log.Printf("%30s %s %s", display, client.MAC, ip)
+	}
+}
+
+func firstNonEmpty(s ...string) string {
+	for _, candidate := range s {
+		if len(candidate) > 0 {
+			return candidate
+		}
+	}
+
+	return ""
 }
